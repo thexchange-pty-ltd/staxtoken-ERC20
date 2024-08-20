467987d0555216760f64876b7a7eae6fa6b5655f
Omer Salkanovic <osalkanovic5@gmail.com>
2024-08-20 14:56:46 +0200

    feat: erc20 smart contract and harhat setup

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();  // Load environment variables
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    holesky: {
+      url: `https://holesky.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
